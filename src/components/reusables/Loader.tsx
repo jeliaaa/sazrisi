aae74994e2e3099c9e4a54b02e29acf95420d3e6
jeliaaa <julia.aleqsandre@gmail.com>
2025-08-07 19:58:30 +0400

    a

--- src/components/reusables/Loader.tsx
+++ src/components/reusables/Loader.tsx
@@ -2,7 +2,7 @@ import { RingLoader } from "react-spinners"
 
 const Loader = () => {
     return (
-        <div className="w-full h-full flex items-center justify-center">
+        <div className="absolute left-0 top-0 z-100 w-screen h-screen flex items-center justify-center">
             <RingLoader />
         </div>
     )
