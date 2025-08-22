16b60f9f33a6eb76e765a04fa24726c3ae2ea277
jeliaaa <julia.aleqsandre@gmail.com>
2025-08-22 14:25:10 +0400

    a

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,20 +5,22 @@ const Main = () => {
       <header className="w-full bg-main-color/80 p-5 flex items-center justify-center">
         <span className="title text-texts-color">მოგესალმებით!</span>
       </header>
-      <div className="w-full flex">
-        <div className="w-1/2 h-[300px] bg-center bg-no-repeat bg-cover rounded-md" style={{ backgroundImage: `url(${'https://picsum.photos/300/100'})` }} />
-        <div className="w-1/2 flex-col">
-          <div className="w-full flex items-center justify-between plain-text">
-            <span>გათამაშება</span>
-            <span>22.08.2025</span>
-          </div>
-          <h1 className="title">ჩაერთე გათამაშებაში და მოიგე პრიზი!</h1>
-          <div className="plain-text">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, deserunt!</p>
-            <br />
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi at accusamus omnis eligendi excepturi tenetur nihil asperiores minima quas ea?</p>
-            <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti asperiores officiis suscipit non optio eaque illo quaerat tempora. Voluptatem ad illo, ex placeat itaque ratione dolor commodi asperiores perferendis. Consequatur ipsa hic architecto officia ad.</p>
+      <div className="p-5 flex flex-col">
+        <div className="w-full flex border-2 border-dark-color">
+          <div className="w-1/2 h-[300px] bg-center bg-no-repeat bg-cover rounded-md" style={{ backgroundImage: `url(${'https://picsum.photos/300/100'})` }} />
+          <div className="w-1/2 flex-col">
+            <div className="w-full flex items-center justify-between plain-text">
+              <span>გათამაშება</span>
+              <span>22.08.2025</span>
+            </div>
+            <h1 className="title text-main-color text-center">ჩაერთე გათამაშებაში და მოიგე პრიზი!</h1>
+            <div className="plain-text text-dark-color p-5">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, deserunt!</p>
+              <br />
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi at accusamus omnis eligendi excepturi tenetur nihil asperiores minima quas ea?</p>
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti asperiores officiis suscipit non optio eaque illo quaerat tempora. Voluptatem ad illo, ex placeat itaque ratione dolor commodi asperiores perferendis. Consequatur ipsa hic architecto officia ad.</p>
+            </div>
           </div>
         </div>
       </div>
