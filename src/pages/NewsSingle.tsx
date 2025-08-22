96d92379ed5de75bf9d104f02bdb2976a5671aa6
jeliaaa <julia.aleqsandre@gmail.com>
2025-08-23 03:43:23 +0400

    a

--- src/pages/NewsSingle.tsx
+++ src/pages/NewsSingle.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import { useNewsStore } from "../stores/newsStore"
+import { useParams } from "react-router-dom";
+import Loader from "../components/reusables/Loader";
 
 const NewsSingle = () => {
+    const { loading, fetchNewsSingle, newsSingle } = useNewsStore();
+    const { id } = useParams()
+    useEffect(() => {
+        if (id) {
+            fetchNewsSingle(parseInt(id))
+        }
+    }, [id, fetchNewsSingle])
+
+    console.log(newsSingle);
+
+    if (loading) {
+        return <Loader />
+    }
+
     return (
         <div>NewsSingle</div>
     )
