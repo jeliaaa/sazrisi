0a86951da970f17c256c45a28ceb7cc7490f7bd4
jeliaaa <julia.aleqsandre@gmail.com>
2025-08-22 14:31:16 +0400

    a

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,7 +3,7 @@ const Main = () => {
   return (
     <div className="w-full min-h-screen flex flex-col">
       <div className="p-5 flex flex-col ">
-        <div className="w-full flex flex-col items-center h-[400px] bg-main-color rounded-xl">
+        <div className="w-full flex flex-col py-5 items-center h-[400px] bg-main-color rounded-xl">
           <span className="text-7xl text-texts-color">გათამაშება უკვე დაიწყო!</span>
         </div>
       </div>
