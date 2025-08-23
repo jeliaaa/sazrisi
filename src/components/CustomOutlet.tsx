2dffc7ef9f434e25595b5b9f09ef5a47534e7121
jeliaaa <julia.aleqsandre@gmail.com>
2025-08-23 04:02:33 +0400

    a

--- src/components/CustomOutlet.tsx
+++ src/components/CustomOutlet.tsx
@@ -7,7 +7,7 @@ function CustomOutlet() {
     return (
         <div className='w-full min-h-dvh items-center justify-center flex relative bg-white'>
             <Navigation />
-            <div className="w-full md:w-[95dvw] pb-30 md:pb-0">
+            <div className="w-full md:w-[95dvw] md:ml-[5dvw] pb-30 md:pb-0">
                 <Outlet />
             </div>
         </div>
