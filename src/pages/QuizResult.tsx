7330c5c34858a70992c96e94f08b023532a4164f
jeliaaa <julia.aleqsandre@gmail.com>
2025-08-23 23:11:37 +0400

    a

--- src/pages/QuizResult.tsx
+++ src/pages/QuizResult.tsx
@@ -5,7 +5,7 @@ import CkeditorContentViewer from '../components/CkEditorContentViewer';
 import { useAttemptStore } from '../stores/attemptStore';
 import { Question } from '../types/types';
 import PDFViewer from '../components/PdfViewer'; // 👈 import your PDF viewer
-import test from '../../public/test.pdf'
+import test from '../assets/test.pdf'
 
 const QuizResultPage = () => {
     const { attemptId } = useParams();
