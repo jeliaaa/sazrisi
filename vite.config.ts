2a39eef3eda5589583239c38a6aea195efebe072
jeliaaa <julia.aleqsandre@gmail.com>
2025-09-22 23:52:38 +0400

    a

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   server: {
     host: true, 
     port: 5173,
-    allowedHosts: ['sazrisi.ge', 'localhost', '127.0.0.1'], 
+    allowedHosts: ['app.sazrisi.ge', 'localhost', '127.0.0.1'], 
   },
 })
