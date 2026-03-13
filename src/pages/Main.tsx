c632060ec133df56f0e3ddb549e599920f7f162a
jeliaaa <julia.aleqsandre@gmail.com>
2026-03-13 17:06:46 +0400

    banner

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import leaderboardImg from "../icons/leaderboard.png"
 import { MainNewsComponent } from "./News";
 
@@ -5,11 +6,11 @@ const Main = () => {
   return (
     <div className="w-full min-h-screen box-border flex flex-col">
       <div className="p-5 flex flex-col ">
-        <div className="w-full text-texts-color flex flex-col py-5 px-3 items-center gap-y-5 h-fit lg:h-[400px] bg-main-color cursor-pointer rounded-xl hover:scale-102 transition-all delay-200">
-          <span className="text-center text-4xl md:text-7xl">გათამაშება უკვე დაიწყო!</span>
-          <span className="text-center title">დააჭირეთ ბანერს დამატებითი ინფორმაციის მისაღებად</span>
+        <Link to="/imitated" className="w-full text-texts-color flex flex-col py-5 px-3 items-center gap-y-5 h-fit lg:h-[400px] bg-main-color cursor-pointer rounded-xl hover:scale-102 transition-all delay-200">
+          <span className="text-center text-4xl md:text-7xl">დარეგისტრირდი იმიტირებულ გამოცდაზე!</span>
+          <span className="text-center title">დააჭირე ბანერს იმიტირებულ გამოცდაზე რეგისტრაციის დასაწყებად.</span>
           <img src={leaderboardImg} alt="..." />
-        </div>
+        </Link>
         <p className="mt-10 title text-dark-color">ახალი ამბები</p>
         <MainNewsComponent />
       </div>
